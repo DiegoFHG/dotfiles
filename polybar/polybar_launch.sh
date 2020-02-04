2b93e0560da8e463f47be31e1ddcfcc342693f51
DiegoFHG <diego.francisco2@alumnos.uneatlantico.es>
2020-02-04 14:01:04 +0100

    Added configuration for double monitor.

--- polybar/polybar_launch.sh
+++ polybar/polybar_launch.sh
@@ -7,16 +7,18 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 MONITORS_COUNT=$(eval "xrandr -q | grep -o '\bconnected\b' | cut -d ' ' -f1 | wc -l")
-MONITORS_NAME=($(eval "xrandr --query | grep '\bconnected\b' | cut -d ' ' -f1 | awk '{ print$1 }'"))
+MONITORS_NAME_STRING=$(eval "xrandr -q | grep '\bconnected\b' | cut -d ' ' -f1 | awk '{ print$1 }'")
+MONITORS_NAME_ARRAY=($(echo "$MONITORS_NAME_STRING" | tr ',' '\n'))
 
 if (( "$MONITORS_COUNT" <= 1 )); then
   MONITOR_0=$MONITORS_NAME
 else
-  for monitor in $(seq 0 "$(($MONITORS_COUNT-1))")
+  for (( x=0; x<($MONITORS_COUNT); x++ ))
   do
-    export "MONITOR_$monitor=$MONITORS_NAME[$monitor]"
+    export "MONITOR_$x"="${MONITORS_NAME_ARRAY[$x]}"
   done
 fi
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar myBar &
+polybar myBar2 &
